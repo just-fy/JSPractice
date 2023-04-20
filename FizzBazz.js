4c5f6bf07298628b0d68a3e1c2d6a8aa46929560
Dariastep <123622878+Dariastep@users.noreply.github.com>
2023-04-20 10:59:40 +0200

    The alternative solution with the help of thr ternary operator.

--- FizzBazz.js
+++ FizzBazz.js
@@ -1,14 +1,11 @@
-const fizzBuzz = () => {
-    console.log('text');
-    /* add alternative solution (ternary operator)
+function fizzBuzz() {
+     /* add alternative solution (ternary operator) */
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzBuzz');
-        } else if (i % 5 === 0) {
-            console.log('buzz');
-        } else if (i % 3 === 0) {
-            console.log('fizz');
-        } else console.log(i);
-    } */
+    console.log((i % 3 === 0 && i % 5 === 0) ? 'fizzBuzz'
+    : (i % 5 === 0) ? 'buzz'
+    : (i % 3 === 0) ? 'fizz'
+    : i);
+    }
+     
 }
 fizzBuzz(); 
